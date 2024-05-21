89c5379df7a337a5611db3618290c4c3aa816a8f
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-05-21 14:28:56 +0200

    added resetGame() function that deletes all the boxes from the board

--- main.js
+++ main.js
@@ -12,6 +12,10 @@ board.appendChild(temp) // place l'élément k pointé temporairement à la fin 
 }
 */
 
+function isNumber(value) {
+    return !isNaN(parseInt(value)) && isFinite(value);
+}
+
 function shuffleChildren(parent) {
     let children = parent.children
     let i = children.length, k, temp
@@ -32,48 +36,91 @@ function showReaction(type, clickedBox) {
     } 
 }
 
+function resetGame() {
+    document.querySelectorAll(".box").forEach(el => el.remove())
+}
+// alternative using vanilla Javascript : 
+// while (boxes.length > 0) boxes[0].remove();
+
+// version plus précise (qui permet de cibler les éléments enfants depuis un certain parent) :
+/* function removeElementsByClass(className){
+    const elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+} */
+
+
 const box = document.createElement("div")
 box.classList.add("box")
 
 const board = document.querySelector("#board")
-//board.appendChild(box)
-//box.innerText = 1;
 
-let nb = 1;
+let boxes = document.getElementsByClassName("box");
 
-for(let i = 1; i <= 10; i++) {
-    let newbox = box.cloneNode()
-    newbox.innerText = i
-    board.appendChild(newbox)
 
-    newbox.addEventListener("click", function() {
+function startGame() {
 
-        if (i == nb) {
-            newbox.classList.add("box-clicked")
-            // 1
-            if(nb == board.children.length) {
-                board.querySelectorAll(".box").forEach(function(box) {
-                    showReaction("success", box)
-                })
-            }
-            nb++
-            shuffleChildren(board)
-        }
-        //2
-        else if (i > nb){
-            showReaction("error", newbox)
-            nb = 1
-            board.querySelectorAll(".box-clicked").forEach(function(validBox) {
-                validBox.classList.remove(("box-clicked"))
-            })
-            shuffleChildren(board)
-        }
-        //3
-        else {
-            showReaction("notice", newbox)
-        }
+    resetGame();
+    let numberOfBoxes = 0;
+    
+    board.querySelectorAll(".box-clicked").forEach(function(validBox) {
+        validBox.classList.remove(("box-clicked"))
     })
+    
+     numberOfBoxes = prompt("How many boxes?")
+     //console.log(numberOfBoxes);
+     console.log(board);
+
+    if (numberOfBoxes != null && isNumber(numberOfBoxes) == true) { 
+        
+        let nb = 1;
+
+        for(let i = 1; i <= numberOfBoxes; i++) {
+            let newbox = box.cloneNode()
+            newbox.innerText = i
+            board.appendChild(newbox)
+        
+            newbox.addEventListener("click", function() {
+        
+                if (i == nb) {
+                    newbox.classList.add("box-clicked")
+                    // 1
+                    if(nb == board.children.length) {
+                        board.querySelectorAll(".box").forEach(function(box) {
+                            showReaction("success", box)
+                        })
+                    }
+                    nb++
+                    shuffleChildren(board)
+                }
+                //2
+                else if (i > nb){
+                    showReaction("error", newbox)
+                    nb = 1
+                    board.querySelectorAll(".box-clicked").forEach(function(validBox) {
+                        validBox.classList.remove(("box-clicked"))
+                    })
+                    shuffleChildren(board)
+                }
+                //3
+                else {
+                    showReaction("notice", newbox)
+                }
+            })
+        }
+    } else {
+        nb = 0;
+        startGame();
+    }
 }
+
+
+
+
+
+ 
+
 
 
 
