3101e2f713caedde56b45e153ce652836287bf8a
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-05-21 10:01:22 +0200

    updt

--- main.js
+++ main.js
@@ -23,6 +23,15 @@ function shuffleChildren(parent) {
     }
 }
 
+function showReaction(type, clickedBox) {
+    clickedBox.classList.add(type)
+    if(type !== "success"){
+        setTimeout(function(){
+            clickedBox.classList.remove(type)
+        }, 800)
+    } 
+}
+
 const box = document.createElement("div")
 box.classList.add("box")
 
@@ -30,18 +39,45 @@ const board = document.querySelector("#board")
 //board.appendChild(box)
 //box.innerText = 1;
 
+let nb = 1;
+
 for(let i = 1; i <= 10; i++) {
     let newbox = box.cloneNode()
     newbox.innerText = i
     board.appendChild(newbox)
 
     newbox.addEventListener("click", function() {
-        console.log("Boite N°"+ i +", click!")
-        newbox.classList.add("box-clicked")
+
+        if (i == nb) {
+            newbox.classList.add("box-clicked")
+            // 1
+            if(nb == board.children.length) {
+                board.querySelectorAll(".box").forEach(function(box) {
+                    showReaction("success", box)
+                })
+            }
+            nb++
+            shuffleChildren(board)
+        }
+        //2
+        else if (i > nb){
+            showReaction("error", newbox)
+            nb = 1
+            board.querySelectorAll(".box-clicked").forEach(function(validBox) {
+                validBox.classList.remove(("box-clicked"))
+            })
+            shuffleChildren(board)
+        }
+        //3
+        else {
+            showReaction("notice", newbox)
+        }
     })
 }
 
-// d. Prendre en charge les phases de jeu
+
+
+
 
 
 
